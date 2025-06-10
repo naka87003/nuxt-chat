c59519b7b5f980ab212950847afdbfd6b303c53c
naka87003 <naka87003@outlook.jp>
2025-06-11 07:46:23 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/test-utils", "@nuxt/ui"],
+  future: {
+    compatibilityVersion: 4,
+  },
+});
