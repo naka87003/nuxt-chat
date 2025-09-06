f58c35c556666f6b176c92d4a52c50ab3243eb3a
naka87003 <naka87003@outlook.jp>
2025-09-06 17:20:06 +0900

    GitHubのavatarが読み込めてなかったので、許可ドメイン設定を追加

--- layers/auth/nuxt.config.ts
+++ layers/auth/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  image: {
+    domains: ["avatars.githubusercontent.com"],
+  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["nuxt-auth-utils"],
